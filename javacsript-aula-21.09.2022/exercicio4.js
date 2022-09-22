d8983da742ec3d329754456e672406a0d320c6d5
Leticia Leal <leticia.leal@growdev.com.br>
2022-09-21 22:46:15 -0300

    Co-authored-by: leticiarutsatz <LeticiaRutsatz@users.noreply.github.com> Co-authored-by: Ramon Barth <RamonBarth1@users.noreply.github.com> Co-authored-by: Gabriel Pacheco <Pacheco-gbr@users.noreply.github.com>

--- javacsript-aula-21.09.2022/exercicio4.js
+++ javacsript-aula-21.09.2022/exercicio4.js
@@ -6,3 +6,20 @@ que diga se ela poderá ou não votar este ano (não é necessário
 considerar o mês em que a pessoa nasceu).
 
 */
+
+/* const timestamp = new Date()
+
+let anoAtual = timestamp.getFullYear() */
+
+let anoAtual = Number(prompt("Por favor, digite o ano atual: "))
+let anoNascimento = Number(prompt("Por favor, digite o seu ano de nascimento: "))
+
+if(anoAtual - anoNascimento < 0) {
+  console.log("Por favor, digite valores adequados.")
+} else if(anoAtual - anoNascimento >= 16) {
+  console.log("Você já pode votar.")
+} else {
+  console.log("Você não pode votar.")
+}
+
+
