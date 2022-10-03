4ec04642e7b23319bcb0d0c79e8bb9a6f1ba7d00
Leticia Leal <leticia.leal@growdev.com.br>
2022-10-03 19:10:03 -0300

    desafio

---
+++ javascript-03.10.2022/index.js
@@ -0,0 +1,19 @@
+import produtos from "./database.js";
+
+console.log(produtos);
+
+// REGRA - unica variavel a ser utilizada é produtos
+// REGRA - não pode alterar a variavel 'produtos'
+
+// 1. Buscar e imprimir todos os produtos que estão com estoque 0
+
+// 2. Buscar e imprimir todos os produtos que possuirem uma margem de lucro acima de 40%
+
+// 3. Buscar todas as BATEDEIRAS da cor 'azul' e imprimir por ordem de valor (do menor ao maior)
+
+// 4. Buscar todas as BATEDEIRAS que possuirem estoque igual ou maior que 5, adicionar um desconto de 15% em seu valor de venda e imprimir a lista de produto com o novo valor
+
+
+// 5. Imprimir o valor de estoque da loja - todos os produtos somados (precoVenda)
+
+// 6. Imprimir a quantidade total de produtos em estoque (estoque)
